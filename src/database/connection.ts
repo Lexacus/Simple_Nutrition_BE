53bfac7b5e08939a641f47f7052f943cfd4a4011
Lexacus <aleserr894@gmail.com>
2024-07-15 10:48:41 +0200

    refactor: obscure connection string again

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectToDatabase = () => {
-  /*  if (!process.env.MONGO_URI) {
+  if (!process.env.MONGO_URI) {
     console.error("There was a problem with provided MongoDB URI");
     return;
-  } */
-  mongoose.connect(
-    "mongodb+srv://aleserrgwe:VSSlQhfgluvv74YgKvUs@cluster0.0szffmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  }
+  mongoose.connect(process.env.MONGO_URI);
 };
